f6563de62a6c195b14eb443418396834f851c65f
Ian Wilson <wilsonia@oregonstate.edu>
2018-09-07 14:33:00 -0700

    follow PEP8 naming

--- roles/docker-garbage-collection/files/garbage-collect.py
+++ roles/docker-garbage-collection/files/garbage-collect.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env  python
-
-# collect-garbage.py - docker garbage collection script
+# collect_garbage.py - docker garbage collection script
 # usage: python app.py CONFIG_FILE_PATH
 
 import os
@@ -19,73 +18,73 @@ def main():
         config = yaml.load(yml)
 
     # get dangling IDs
-    danglingIDs = getDanglingIDs()
+    dangling_ids = get_dangling_ids()
 
     # filter IDs for images older than 2 weeks
-    danglingIDs = selectOldIDs(config, danglingIDs)
+    dangling_ids = select_old_ids(config, dangling_ids)
 
     # backup images
-    backupImages(config, danglingIDs)
+    backup_images(config, dangling_ids)
 
     # remove dangling images and broken containers
-    garbageCollect(config, danglingIDs)
+    collect_garbage(config, dangling_ids)
 
 
-def getDanglingIDs():
+def get_dangling_ids():
     """Collect a list of image IDs for dangling images
     (unused by any functional container)"""
     try:
         # output of docker command is in format of one ID per line,
         # so splitline is used for parsing.
-        danglingIDs = subprocess.check_output(
+        dangling_ids = subprocess.check_output(
             ['docker images --filter "dangling=true" --format "{{.ID}}"'],
             shell=True)
     except subprocess.CalledProcessError as e:
         sys.exit('Error getting IDs of dangling images: ' + e.output)
-    return str.splitlines(danglingIDs)
+    return str.splitlines(dangling_ids)
 
 
-def selectOldIDs(config, danglingIDs):
+def select_old_ids(config, dangling_ids):
     """Inspect each image, and return list of images that are older than
     configured minimum. Docker does not have a filter by time argument for
     listing images, only pruning them."""
-    oldIDs = []
-    for id in danglingIDs:
+    old_ids = []
+    for id in dangling_ids:
         try:
-            idDetails = json.loads(
+            id_details = json.loads(
                 subprocess.check_output(['docker inspect ' + id], shell=True))
             # Docker varies the precision/format used in the Created field,
             # so parsing the datetime requires the dateutil library.
-            idCreatedDate = parser.parse(idDetails[0]['Created'])
-            if (
-                datetime.now(idCreatedDate.tzinfo)-relativedelta(
-                    hours=config['dangling_image_removal_minimum_age_hours']).
-                    normalized() > idCreatedDate
-                    ):
-                    oldIDs.append(id)
+            id_created_date = parser.parse(id_details[0]['Created'])
+            threshold_date = datetime.now(
+                id_created_date.tzinfo) - relativedelta(
+                    hours=config['dangling_image_removal_minimum_age_hours'].
+                    normalized())
+            if threshold_date > id_created_date:
+                old_ids.append(id)
         except subprocess.CalledProcessError as e:
             sys.exit('Error inspecting image ' + id + ": " + e.output)
-    return oldIDs
+    return old_ids
 
 
-def backupImages(config, danglingIDs):
+def backup_images(config, dangling_ids):
     """Backs up a collection of images"""
-    for id in danglingIDs:
-        backupImage(id, config['backup_images'])
+    for id in dangling_ids:
+        backup_image(id, config['backup_images'])
 
 
-def backupImage(imageID, path):
+def backup_image(image_id, path):
     """Backup image as .tar in configured backup image directory"""
-    print ("Backing up " + imageID)
+    print ("Backing up " + image_id)
     try:
         subprocess.check_output(
-            ['docker save --output ' + path + imageID + ".tar " + imageID],
+            ['docker save --output ' + path + image_id + ".tar " + image_id],
             shell=True)
     except subprocess.CalledProcessError as e:
-        sys.exit('Error backing up image ' + imageID + ": " + e.output)
+        sys.exit('Error backing up image ' + image_id + ": " + e.output)
 
 
-def garbageCollect(config, danglingIDs):
+def collect_garbage(config, dangling_ids):
     """ Removes images and corresponding broken containers.
     Begins by removing the passed images.
     If an error occurs during this process involving a stopped container, this
@@ -94,51 +93,52 @@ def garbageCollect(config, danglingIDs):
     attempting to remove the image again.
     This is the clearest way to determine if a container is "bad"
     """
-    for id in danglingIDs:
-        output = removeImage(id)
+    for id in dangling_ids:
+        output = remove_image(id)
         while "stopped container" in output[1]:
-            containerID = output[1].split("stopped container ", 1)[1].strip()
-            backupContainer(config, containerID, id)
-            removeContainer(containerID)
-            output = removeImage(id)
+            container_id = output[1].split("stopped container ", 1)[1].strip()
+            backup_container(config, container_id, id)
+            remove_container(container_id)
+            output = remove_image(id)
 
 
-def removeImage(imageID):
+def remove_image(image_id):
     """Removes an image.
     Returns the error STDOUT and STDERR outputs.
     The STDERR output is required for handling the stopped container error."""
     try:
         removal = subprocess.Popen(
-            ['docker rmi ' + imageID],
+            ['docker rmi ' + image_id],
             shell=True, stdout=subprocess.PIPE,
             stderr=subprocess.PIPE)
         return removal.communicate()
     except Exception as e:
-        sys.exit('Error removing image ' + imageID + ": " + e)
+        sys.exit('Error removing image ' + image_id + ": " + e)
 
 
-def backupContainer(config, containerID, imageID):
+def backup_container(config, container_id, image_id):
     """ function description """
     try:
         output = subprocess.check_output(
-            ['docker commit ' + containerID + ' broken-container:' + imageID],
+            ['docker commit ' + container_id + ' broken-container:'
+                + image_id],
             shell=True)
         output = output.split("sha256:", 1)[1].strip()
-        backupPath = config['backup_containers'] + '/' + imageID + '/'
-        if not os.path.exists(backupPath):
-            os.makedirs(backupPath)
-        backupImage(output, backupPath)
-        removeImage(output)
+        backup_path = config['backup_containers'] + '/' + image_id + '/'
+        if not os.path.exists(backup_path):
+            os.makedirs(backup_path)
+        backup_image(output, backup_path)
+        remove_image(output)
     except subprocess.CalledProcessError as e:
-        sys.exit('Error backing up container ' + containerID + ": " + e.output)
+        sys.exit('Error container backup: ' + container_id + ": " + e.output)
 
 
-def removeContainer(containerID):
+def remove_container(container_id):
     """Removes a container"""
     try:
-        subprocess.check_output(['docker rm ' + containerID], shell=True)
+        subprocess.check_output(['docker rm ' + container_id], shell=True)
     except subprocess.CalledProcessError as e:
-        sys.exit('Error removing container ' + containerID + ": " + e.output)
+        sys.exit('Error removing container ' + container_id + ": " + e.output)
 
 
 main()
